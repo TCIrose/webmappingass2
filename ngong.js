67fb1a1a26467332156932519c1f23ca2c4fa6cb
Tony Irose <57923336+TCIrose@users.noreply.github.com>
2020-05-01 01:31:29 +0300

    Added Interactivity
    Buttons added to make it easier to display specific points.

--- ngong.js
+++ ngong.js
@@ -2,7 +2,7 @@
 var nmap = document.getElementById("map");
 var map = L.map(nmap, {
     center: [-1.358609, 36.656285],
-    zoom: 17,
+    zoom: 15,
     trackResize: true,
     boxZoom: true,
     scrollWheelZoom: true,
@@ -11,13 +11,15 @@ var map = L.map(nmap, {
 //add tile (base map)
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", { attribution: "© OpenStreetMap" }).addTo(map);
 
-//adding points
-var milelem = L.marker([-1.358202, 36.65703]).addTo(map);
-var pchurch = L.marker([-1.357293, 36.657686]).addTo(map);
-var nba = L.marker([-1.358609, 36.656285]).addTo(map);
-var naivas = L.marker([-1.360946, 36.656529]).addTo(map);
-//adding interactivity with popups
-milelem.bindPopup("<h3>Milele Mall</h3><p><img src='images/milele mall.jpg' align = 'left'><br clear ='left'/>Ample parking space available.</p>");
-pchurch.bindPopup("<h3>PCEA Enchoro Emuny Church</h3><p><img src='images/pcea.jpg'>Sunday Services from 8:00am</p>");
-nba.bindPopup("<h3>Ngong' Business Arcade</h3><p><img src='images/nba.jpg'></p>");
-naivas.bindPopup("<h3>Naivas Supermarket</h3><p><img src='images/naivas.jpg'></p>");
+//display longitude and latitude onclick 
+var longlat = L.popup();
+
+function onMapClick(e) {
+    longlat
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on("click", onMapClick);
+
+//adding GeoJSON points and binding them with popups
